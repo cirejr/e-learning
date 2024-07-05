014eef1b2b98762fd5dc1205a2f193399e00b1ae
cirejr <juniorcireba@gmail.com>
2024-07-05 13:13:48 +0000

    update

--- front/components/auth/right-side.tsx
+++ front/components/auth/right-side.tsx
@@ -18,7 +18,7 @@ export default function RightSide() {
         >
           <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
         </svg>
-        <Link href='/'>Acceuil</Link>
+        <Link href='/'>E-Learning</Link>
       </div>
     </div>
   );
